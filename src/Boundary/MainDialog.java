e56e4f53f3829e57ffc615502bf91f5ee734cc46
HTRTan <tan@htrtan>
2018-12-01 23:35:42 +0800

    import java project

---
+++ src/Boundary/MainDialog.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Boundary;
+
+/**
+ * @author lenovo
+ *
+ */
+public class MainDialog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
